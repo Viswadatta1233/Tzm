984bc7a0ac017730f49966c0e8da3bf017b5e83a
Geervaniniudumukkala <geervani1825@gmail.com>
2025-01-26 17:21:37 +0530

    nm

--- VM/server.js
+++ VM/server.js
@@ -35,5 +35,5 @@ app.use('/api/messages', messageRoutes);
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT} and hi`);
 });
