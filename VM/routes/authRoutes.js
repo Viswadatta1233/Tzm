7c1b1d8c1b26d4f6501a72731fef86e95fe3b6fa
Geervaniniudumukkala <geervani1825@gmail.com>
2025-03-07 17:09:20 +0530

    restriction

--- VM/routes/authRoutes.js
+++ VM/routes/authRoutes.js
@@ -6,11 +6,11 @@ const { upload } = require('../config/cloudinary');
 
 const router = express.Router();
 
-router.post('/add', authenticate, isAuthorized, addUser); // Allow Admins and Coordinators
+router.post('/add', addUser); // Allow Admins and Coordinators
 router.post('/login', loginUser);
 router.post('/logout', authenticate, logoutUser);
-router.post('/upload-photo', authenticate, upload.single('photo'), uploadPhoto);
-router.get('/volunteers', authenticate,isAuthorized,viewVolunteers);
+router.post('/upload-photo', upload.single('photo'), uploadPhoto);
+router.get('/volunteers',viewVolunteers);
 router.put('/volunteers/:tzId', updateVolunteer);
 router.delete('/volunteers/:tzId',deleteVolunteer);
 
