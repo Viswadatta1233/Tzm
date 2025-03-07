016d6b2e6e0f9cad0742f19fce1c098b4a80ed35
Geervaniniudumukkala <geervani1825@gmail.com>
2025-03-07 17:19:02 +0530

    restriction2

--- VM/models/User.js
+++ VM/models/User.js
@@ -4,11 +4,11 @@ const userSchema = new mongoose.Schema({
     tzId: { type: String, unique: true, required: true },
     password: { type: String, required: true },
     name: { type: String, required: true }, // Added name field
-    branch: { type: String, required: true },
+    branch: { type: String},
     year: { type: Number, required: true },
-    phone: { type: String, required: true },
-    club: { type: String, required: true },
-    role: { type: String, enum: ['Volunteer', 'Core Team', 'Admin'], required: true },
+    phone: { type: String},
+    club: { type: String},
+    role: { type: String, enum: ['Volunteer', 'Core Team', 'Admin']},
     photo: { type: String ,default:''},
     creditScore: { type: Number, default: 0 },
     original:{ type: String}
